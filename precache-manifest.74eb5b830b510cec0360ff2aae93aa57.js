2366686b2f110c700b40b260737bcc45bf39d21c
Jimmy Tang <jimmytang@Jimmyde-Mac-mini.local>
2019-06-24 13:36:17 +0800

    Updates

---
+++ precache-manifest.74eb5b830b510cec0360ff2aae93aa57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2349af56c59c63400789cac04b285704",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "f3496a8da22724b0eb20",
+    "url": "/calculator/static/css/main.c303ffa5.chunk.css"
+  },
+  {
+    "revision": "4c95f44f399b6150c266",
+    "url": "/calculator/static/js/2.0007278a.chunk.js"
+  },
+  {
+    "revision": "f3496a8da22724b0eb20",
+    "url": "/calculator/static/js/main.770c01e8.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
